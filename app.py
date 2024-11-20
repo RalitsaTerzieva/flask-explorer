4eac2bb168b03a68f6d343e00f333afccf72d122
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-20 16:02:41 +0200

    Add html and static files

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello, puppy!</h1>"
+    return render_template("base.html")
 
 @app.route('/information')
 def info():
